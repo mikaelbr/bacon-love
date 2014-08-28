747d33e50bc77e8fa4c2f7ae15d66ce5ef16e3df
Stian Veum Møllersen <stian.moellersen@gmail.com>
2014-08-28 14:47:34 +0200

    Better solution for ex15

--- exercises/15_loading_spinner/solution/solution.js
+++ exercises/15_loading_spinner/solution/solution.js
@@ -2,7 +2,5 @@ module.exports = function (clicks, startAsyncTask) {
   var request = clicks.map(true);
   var response = request.flatMap(startAsyncTask);
 
-  var pending = request.merge(response.map(false)).toProperty(false);
-
-  return pending;
+  return request.awaiting(response);
 };
