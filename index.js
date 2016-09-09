22c9233f6387c2aa18d7622da8f6f25601141eca
Mikael Brevik <mikaelbre@gmail.com>
2016-09-09 13:29:25 +0200

    Initial transition to latest workshopper

--- index.js
+++ index.js
@@ -2,16 +2,38 @@
 
 require('./enforce-node-version')();
 
-const workshopper = require('workshopper');
+const workshopper = require('workshopper-adventure');
 const join = require('path').join;
 
-workshopper({
+var w = workshopper({
   name: 'bacon-love',
   title: 'A Workshop for Functional Reactive Programming',
-  exerciseDir: join(__dirname, 'exercises'),
+  // exerciseDir: join(__dirname, 'exercises'),
   appDir: __dirname,
   menu: {
     fg: 'cyan',
     bg: 'black',
   }
-});
+})
+
+
+w.addAll([
+  "01. Intro to the workshop",
+  "02. Wrapping Values as Reactive Datatypes",
+  "03. Subscription Based",
+  "04. EventStreams",
+  "05. Properties",
+  "06. Map and Filter",
+  "07. Fold and Scan",
+  "08. Combining Observables, part I",
+  "09. Combining Observables, part II",
+  "10. And or Or or Maybe Not",
+  "11. Sampled",
+  "12. Selective Data By Timing",
+  "13. Flatmaps",
+  "14. Field and Form Validation",
+  "15. Loading Spinner",
+  "16. Error Handling"
+])
+
+w.execute(process.argv.slice(2));
